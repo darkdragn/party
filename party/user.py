81b0c47f3f71786c7dc5726a4bf4fa27b2de6274
darkdragn <darkdragn.cj@gmail.com>
2022-01-27 10:17:19 -0700

    feat(user): initial draft of marshmallow schema for user data structure

---
+++ party/user.py
@@ -0,0 +1,37 @@
+
+from dataclasses import dataclass, field
+from datetime import datetime
+
+from marshmallow import Schema, fields, post_load
+from .notes import populate_posts
+
+@dataclass
+class User:
+    id: str
+    name: str
+    service: str
+    indexed: datetime = datetime.now()
+    updated: datetime = datetime.now()
+
+    def __eq__(self, other):
+        output = False
+        if self.service == other['service']:
+            if other['id'].isnumeric():
+                output = self.id == other['id']
+            else:
+                output = self.name == other['id']
+        return output
+            
+
+
+
+class UserSchema(Schema):
+    id: str = fields.Str()
+    name: str = fields.Str()
+    service: str = fields.Str()
+    updated = fields.DateTime("%a, %d %b %Y %H:%M:%S %Z")
+    indexed = fields.DateTime("%a, %d %b %Y %H:%M:%S %Z")
+
+    @post_load
+    def create_user(self, data, **kwargs):
+        return User(**data)
